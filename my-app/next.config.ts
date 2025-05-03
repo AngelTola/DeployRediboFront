a90cd345a09e5d2eb362687b728a9bdfca4f26b1
AngelTola <tolaangel606@gmail.com>
2025-05-03 10:33:52 -0400

    octavo intento

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // Asegúrate de que se utilice el output correcto para despliegue
   output: 'standalone',
 };
 
